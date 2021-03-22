45d1d5406ad27864f7298bff5c63fbc1e90f14f6
RanaAhmedRaza001 <80593645+RanaAhmedRaza001@users.noreply.github.com>
2021-03-22 11:11:59 +0500

    Projectile Motion Script Added

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpringJoint2D springJoint;
+    private bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        springJoint = GetComponent<SpringJoint2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        }
+    }
+    private void OnMouseDown()                   // this will be used when we stretch the ball 
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    
+    }
+    private void OnMouseUp()                      // this will be used when we release the ball 
+
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.15f);
+        GetComponent<SpringJoint2D>().enabled = false;
+
+
+    }
+}
